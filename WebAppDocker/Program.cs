4120b2eee5921ce6e3a0787c1f7141ae553da9f3
fulviocanducci <fulviocanducci@hotmail.com>
2026-03-27 14:37:17 -0300

    update!

--- WebAppDocker/Program.cs
+++ WebAppDocker/Program.cs
@@ -2,7 +2,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.Configure<RouteOptions>(c =>
+{
+    c.LowercaseUrls = true;
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,6 +27,5 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
